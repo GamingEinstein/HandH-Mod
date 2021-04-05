28649e3ce3d67e86d2bec9779ea1e75c201a3b6b
GamingEinstein <78991053+GamingEinstein@users.noreply.github.com>
2021-04-05 18:37:44 -0400

    Add files via upload

---
+++ NPCs/HeavenSeeker.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HandHmod.NPCs
+{
+	public class HeavenSeeker : ModNPC
+	{
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Heaven Seeker");
+			Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Crimera];
+		}
+
+		public override void SetDefaults()
+		{
+			npc.width = 32;
+			npc.height = 64;
+			npc.damage = 20;
+			npc.defense = 5;
+			npc.lifeMax = 200;
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCDeath7;
+			npc.lavaImmune = true;
+			npc.noGravity = true;
+			npc.value = 6000f;
+			npc.knockBackResist = 0.5f;
+			npc.aiStyle = 5;
+			npc.noTileCollide = false;
+			aiType = NPCID.Crimera;
+			animationType = NPCID.Crimera;
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			return spawnInfo.player.GetModPlayer<HandHmodPlayer>().ZoneHeaven ? .5f : .5f;
+		}
+		public override void NPCLoot()
+		{
+			Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("HeavenDust"), 5);
+		}
+	}
+}
---
+++ NPCs/HellfireZombie.cs
@@ -0,0 +1,42 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace HandHmod.NPCs
+{
+	public class HellfireZombie : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Hellfire Zombie");
+			Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.Zombie];
+		}
+
+		public override void SetDefaults()
+		{
+			npc.width = 18;
+			npc.height = 40;
+			npc.damage = 200;
+			npc.defense = 15;
+			npc.lifeMax = 1000;
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCDeath2;
+			npc.lavaImmune = true;
+			npc.value = 6000f;
+			npc.knockBackResist = 0.5f;
+			npc.aiStyle = 3;
+			aiType = NPCID.Zombie;
+			animationType = NPCID.Zombie;
+		}
+
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			return SpawnCondition.Underworld.Chance * 0.5f;
+		}
+			public override void NPCLoot()
+		{
+			Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("hellfirefragments"), 2);
+			Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("CursedSoulOfHell"), 2);
+		}
+	}
+}
