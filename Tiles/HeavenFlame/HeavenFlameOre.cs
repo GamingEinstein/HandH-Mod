8ef085616bfd6612031aee39348ac940e95625e3
GamingEinstein <78991053+GamingEinstein@users.noreply.github.com>
2021-04-05 18:39:42 -0400

    Add files via upload

---
+++ Tiles/HeavenFlame/HeavenFlameOre.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace HandHmod.Tiles.HeavenFlame
+{
+    public class HeavenFlameOre : ModTile
+    {
+        public override void SetDefaults()
+        {
+            Main.tileSolid[Type] = true;
+            Main.tileMergeDirt[Type] = true;
+            Main.tileBlockLight[Type] = true;
+
+            drop = ItemType<Items.Placeables.OreBars.HeavenFlame.HeavenFlameOre>();
+
+            ModTranslation name = CreateMapEntryName();
+            AddMapEntry(Microsoft.Xna.Framework.Color.LightCyan);
+
+            minPick = 200;
+        }
+    }
+}
