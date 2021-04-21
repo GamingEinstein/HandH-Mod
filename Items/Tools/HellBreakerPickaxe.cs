6014be40c8c4d1c355be320569a8b34a61605e5a
GamingEinstein <78991053+GamingEinstein@users.noreply.github.com>
2021-04-21 16:05:52 -0400

    Re-Uploaded for Constency

--- Items/Tools/HellBreakerPickaxe.cs
+++ Items/Tools/HellBreakerPickaxe.cs
@@ -1,8 +1,8 @@
 ﻿using HandHmod.Items.Placeables.OreBars.HellFireFrag;
-using Terraria.ModLoader;
 using Microsoft.Xna.Framework;
-using Terraria.ID;
 using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
 namespace HandHmod.Items.Tools
