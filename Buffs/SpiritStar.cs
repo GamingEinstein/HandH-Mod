0493ba7b0fc6298221d53a4f409e4afbf9cc37ea
GamingEinstein <78991053+GamingEinstein@users.noreply.github.com>
2021-04-21 12:39:16 -0400

    Readded for bug fixing

--- Buffs/SpiritStar.cs
+++ Buffs/SpiritStar.cs
@@ -4,31 +4,31 @@ using Terraria.ModLoader;
 namespace HandHmod.Buffs
 {
     public class SpiritStar : ModBuff
-	{
-		public override void SetDefaults()
-		{
-			DisplayName.SetDefault("Spirit Star");
-			Description.SetDefault("The Spirit Star will fight for you");
-			Main.buffNoSave[Type] = true;
-			Main.buffNoTimeDisplay[Type] = true;
-		}
+    {
+        public override void SetDefaults()
+        {
+            DisplayName.SetDefault("Spirit Star");
+            Description.SetDefault("The Spirit Star will fight for you");
+            Main.buffNoSave[Type] = true;
+            Main.buffNoTimeDisplay[Type] = true;
+        }
 
-		public override void Update(Player player, ref int buffIndex)
-		{
-			HandHmodPlayer modPlayer = player.GetModPlayer<HandHmodPlayer>();
-			if (player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.MightOfTheMinion>()] > 0)
-			{
-				modPlayer.MightOfTheMinion = true;
-			}
-			if (!modPlayer.MightOfTheMinion)
-			{
-				player.DelBuff(buffIndex);
-				buffIndex--;
-			}
-			else
-			{
-				player.buffTime[buffIndex] = 18000;
-			}
-		}
-	}
+        public override void Update(Player player, ref int buffIndex)
+        {
+            HandHmodPlayer modPlayer = player.GetModPlayer<HandHmodPlayer>();
+            if (player.ownedProjectileCounts[ModContent.ProjectileType<Projectiles.MightOfTheMinion>()] > 0)
+            {
+                modPlayer.MightOfTheMinion = true;
+            }
+            if (!modPlayer.MightOfTheMinion)
+            {
+                player.DelBuff(buffIndex);
+                buffIndex--;
+            }
+            else
+            {
+                player.buffTime[buffIndex] = 18000;
+            }
+        }
+    }
 }
--- Generation/Heavens.cs
+++ Generation/Heavens.cs
@@ -1,38 +1,38 @@
-using System.Collections.Generic;
+using HandHmod.Tiles;
+using HandHmod.Tiles.HeavenFlame;
+using HandHmod.Tiles.HeavenTiles;
+using HandHmod.Tiles.Walls;
 using Microsoft.Xna.Framework;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
-using HandHmod.Tiles.HeavenTiles;
-using HandHmod.Tiles.HeavenFlame;
-using HandHmod.Tiles;
-using HandHmod.Tiles.Walls;
 
 namespace HandHmod.Generation
 {
-	public class Heavens
-	{
-		public static void Generate(int x, int y)
-		{
-			GenTiles(x, y);
-		}
+    public class Heavens
+    {
+        public static void Generate(int x, int y)
+        {
+            GenTiles(x, y);
+        }
 
-		private static void GenTiles(int x, int y)
-		{
-			Dictionary<Color, int> colorToTile = new Dictionary<Color, int>
-			{
-				[new Color(0, 255, 255)] = ModContent.TileType<HeavenTile>(),
-				[new Color(0, 100, 100)] = ModContent.TileType<HeavenFlameOre>(),
-				[new Color(255, 255, 255)] = -1,
-				[Color.Black] = -2
-			};
-			Dictionary<Color, int> colorToWall = new Dictionary<Color, int>
-			{
-				[new Color(0, 200, 200)] = ModContent.WallType<HeavenlyWall>(),
-				[Color.Black] = -2
-			};
-			TexGen texGenerator = BaseWorldGenTex.GetTexGenerator(ModContent.GetTexture("HandHmod/Generation/Heavens"), colorToTile, ModContent.GetTexture("HandHmod/Generation/HeavensWalls"), colorToWall, ModContent.GetTexture("HandHmod/Generation/HeavensWater"));
-			texGenerator.Generate(x - texGenerator.width / 2, y - texGenerator.height / 2, silent: true, sync: true);
-			WorldGen.PlaceChest(x - 6, y + 229, (ushort)ModContent.TileType<HeavenlyChest>(), false, 0);
-		}
-	}
+        private static void GenTiles(int x, int y)
+        {
+            Dictionary<Color, int> colorToTile = new Dictionary<Color, int>
+            {
+                [new Color(0, 255, 255)] = ModContent.TileType<HeavenTile>(),
+                [new Color(0, 100, 100)] = ModContent.TileType<HeavenFlameOre>(),
+                [new Color(255, 255, 255)] = -1,
+                [Color.Black] = -2
+            };
+            Dictionary<Color, int> colorToWall = new Dictionary<Color, int>
+            {
+                [new Color(0, 200, 200)] = ModContent.WallType<HeavenlyWall>(),
+                [Color.Black] = -2
+            };
+            TexGen texGenerator = BaseWorldGenTex.GetTexGenerator(ModContent.GetTexture("HandHmod/Generation/Heavens"), colorToTile, ModContent.GetTexture("HandHmod/Generation/HeavensWalls"), colorToWall, ModContent.GetTexture("HandHmod/Generation/HeavensWater"));
+            texGenerator.Generate(x - texGenerator.width / 2, y - texGenerator.height / 2, silent: true, sync: true);
+            WorldGen.PlaceChest(x - 6, y + 229, (ushort)ModContent.TileType<HeavenlyChest>(), false, 0);
+        }
+    }
 }
